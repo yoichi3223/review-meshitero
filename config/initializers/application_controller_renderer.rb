ada1f5febbbab49d09bf335e32f21552ff9300f8
yoichi3223 <yoichi3223@gmail.com>
2020-10-20 06:11:32 +0000

    post_image add

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,5 +1,5 @@
 # Be sure to restart your server when you modify this file.
-
+Refile.secret_key = '48650a01f7a753804fa774b3671c18418c7b44f3264b5019236470f634b2b00454181f6301c36f334d251d28e72b1c9b24743bc8f52ef7abd8ede0dfdaddbf46'
 # ActiveSupport::Reloader.to_prepare do
 #   ApplicationController.renderer.defaults.merge!(
 #     http_host: 'example.org',
