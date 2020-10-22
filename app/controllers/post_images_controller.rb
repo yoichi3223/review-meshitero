318c5429480c246d91a311cc308d13b7e38dd2d3
yoichi3223 <yoichi3223@gmail.com>
2020-10-22 06:51:31 +0000

    image_destroy mypage add

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -11,9 +11,14 @@ class PostImagesController < ApplicationController
     else
       render :new
     end
-
   end
-  
+
+  def destroy
+    @post_image = PostImage.find(params[:id])
+    @post_image.destroy
+    redirect_to root_path
+  end
+
   def show
     @post_image = PostImage.find(params[:id])
     @post_comment = PostComment.new
@@ -21,7 +26,6 @@ class PostImagesController < ApplicationController
   
   def index
     @post_images = PostImage.page(params[:page]).reverse_order
-
   end
   private
   def post_image_params
---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,6 @@
+class UserController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @post_images = @user.post_images.page(params[:page]).reverse_order
+  end
+end
